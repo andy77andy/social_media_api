deccb17481b64e83272e1cbfd8d881056909e6c4
Andy <andy77andy@ukr.net>
2023-09-23 14:50:23 +0300

    Created corresponding urls using router within sm_activity app

---
+++ sm_activity/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from sm_activity.views import (
+    ProfileViewSet,
+    PostViewSet, CommentViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register("profiles", ProfileViewSet)
+router.register("posts", PostViewSet)
+router.register("comments", CommentViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+    ]
+
+app_name = "sm_activity"
